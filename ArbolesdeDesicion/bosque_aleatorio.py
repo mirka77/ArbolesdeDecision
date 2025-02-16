e8116ebcdec42b917ffbab444234d0c708adb207
Mirka <96074272+mirka77@users.noreply.github.com>
2025-02-16 14:49:27 -0700

    TareaArboles

---
+++ ArbolesdeDesicion/bosque_aleatorio.py
@@ -0,0 +1,15 @@
+def entrena_bosque(datos, target, atributos, n_arboles=10, max_profundidad=5):
+    bosque = entrena_bosque(datos, target, clase_default, 
+                        n_arboles=num_arboles, max_profundidad=max_profundidad, 
+                        acc_nodo=acc_nodo, min_ejemplos=min_ejemplos, 
+                        variables_seleccionadas=variables_seleccionadas)
+    pass
+
+def predice_bosque(modelo, datos):
+    prediccion = predice_bosque(bosque, ejemplo)
+    pass
+
+def cargar_datos(ruta):
+    archivo_datos = 'C:\\Users\\virka\\Desktop\\ArbolesdeDesicion\\winequality-red.csv'  
+    datos = cargar_datos(archivo_datos)
+    pass
